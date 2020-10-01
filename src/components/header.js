299e822c2dca3abd7698c473e810256d9947c62e
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-10-01 12:42:33 +0200

    change name to index

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Menu from './Menu/Menu'
+import Menu from './Menu'
 import styled from 'styled-components'
 const StyledHeader = styled.header`
   position: sticky;
   top: 0; 
   z-index: 100;
-  /* background-color: var(--header-background-color); */
   background-color: white;
   color: var(--menu-text-color);
 `
